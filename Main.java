a46cbc8b9d6297cc3cd252be67242c5ad4382dbd
LightProger <photoluxe@mail.ru>
2016-12-25 22:17:50 +0600

    Угадай число

--- Main.java
+++ Main.java
@@ -1,8 +1,26 @@
 package game_guess_the_number;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ваша задача угадать число от 0 до 10");
+        int range = 10;
+        int number = (int)(Math.random() * range);
+        while (true){
+            System.out.println("Введите число: ");
+            int input_number = scanner.nextInt();
+            if(input_number == number){
+                System.out.println("Поздравляем! Вы угадали!!");
+                break;
+            } else if(input_number > number){
+                System.out.println("Загаданное число меньше... ");
+            } else {
+                System.out.println("Загаданное число больше...");
+            }
+        }scanner.close();
     }
 }
